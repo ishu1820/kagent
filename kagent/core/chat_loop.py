6e8a809ee13382e877e6c46ce50e320d0f009d9c
Ishita mahajan <142918056+ishu1820@users.noreply.github.com>
2026-03-07 18:54:10 +0530

    Enhance chat interface with colored user prompts and loading indicator during AI response generation (#15)

--- kagent/core/chat_loop.py
+++ kagent/core/chat_loop.py
@@ -1,7 +1,9 @@
 from kagent.history.convo_memory import ConversationMemory
 from kagent.models.ollama_model import OllamaModel
+from rich.progress import Progress, SpinnerColumn, TextColumn
+from rich.console import Console  #used for different color of user input
 
-
+console = Console()
 # This function starts the interactive chat session
 def start_chat():
     # Create a memory object to store conversation history
@@ -13,7 +15,8 @@ def start_chat():
 
     while True:
 
-        user_input = input("You: ")
+        console.print("[bold red]You:[/bold red] ",end="")
+        user_input = input()
 
         if user_input.lower() in ["exit", "quit"]:
             break
@@ -21,9 +24,13 @@ def start_chat():
          # Add the user's message to conversation memory
         memory.add_user_message(user_input)
 
+        #loader while ai is generating response
         # Send the entire conversation history to the model
         # The model will generate a response based on previous context
-        response = model.generate(memory.get_history())
+        with Progress(SpinnerColumn(), TextColumn("[bold cyan]AI is thinking..."),
+                      transient=True,) as progress:
+            progress.add_task("thinking",total=None)
+            response = model.generate(memory.get_history())
         
         if not response:
             response = "I couldn't generate a response"
